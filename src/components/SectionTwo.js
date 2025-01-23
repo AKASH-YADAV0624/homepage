0d797b4750c852e5f873f18df871b1618252e81a
AKASH-YADAV0624 <117830868+AKASH-YADAV0624@users.noreply.github.com>
2025-01-23 23:34:23 +0530

    Update SectionTwo.js

--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -5,12 +5,12 @@ const SectionTwo=()=>{
         <div className="flex flex-col items-center bg-[#FFFFFF] w-full px-9 py-24  max-sm:px-2 max-sm:py-5">
             <h1 className="text-[#272D37] text-center font-bold text-[22px]">Trusted By Over 100+ Startups and freelance business</h1>
             <div className="flex items-center justify-center gap-7  mt-9 max-lg:flex-wrap ">
-                <img src="images/group.png" alt="company logo" className="w-[137px] " />
-                <img src="images/group1.png" alt="company logo" className="w-[137px] " />
-                <img src="images/group1.png" alt="company logo" className="w-[137px] " />
-                <img src="images/group2.png" alt="company logo" className="w-[137px] " />
-                <img src="images/group3.png" alt="company logo" className="w-[137px] " />
-                <img src="images/group4.png" alt="company logo" className="w-[137px] " />
+                <img src="images/Group.png" alt="company logo" className="w-[137px] " />
+                <img src="images/Group1.png" alt="company logo" className="w-[137px] " />
+                <img src="images/Group1.png" alt="company logo" className="w-[137px] " />
+                <img src="images/Group2.png" alt="company logo" className="w-[137px] " />
+                <img src="images/Group3.png" alt="company logo" className="w-[137px] " />
+                <img src="images/Group4.png" alt="company logo" className="w-[137px] " />
             </div>
         </div>
     )
